bb10d4b4cf3a5efe045575c09ab71ebd7853105a
Jules Kourelakos <julia.kourelakos@duke.edu>
2023-02-23 19:54:21 -0500

    rps error handling

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -28,4 +28,26 @@ if (args.r || args.rules) {
     console.log(`  - Rock CRUSHES Scissors`);
     process.exit(0);
 }
-console.log(JSON.stringify(rps(args._[0])));
+
+try { 
+    console.log(JSON.stringify(rps(args._[0])));
+  }
+  catch (error) {
+    console.log(`Usage: node-rps [SHOT]`);
+    console.log(`Play Rock Paper Scissors (RPS)`);
+    console.log(``);
+    console.log(`  -h, --help      display this help message and exit`);
+    console.log(`  -r, --rules     display the rules and exit`);
+    console.log(``);
+    console.log(`Examples:`);
+    console.log(`  node-rps        Return JSON with single player RPS result.`);
+    console.log(`                  e.g. {"player":"rock"}`);
+    console.log(`  node-rps rock   Return JSON with results for RPS played against a simulated opponent.`);
+    console.log(`                  e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+    console.log(`Rules for Rock Paper Scissors:`);
+    console.log(``);
+    console.log(`  - Scissors CUTS Paper`);
+    console.log(`  - Paper COVERS Rock`);
+    console.log(`  - Rock CRUSHES Scissors`);
+    process.exit(0);                   
+  }

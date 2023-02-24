224830f51dbd569303e85eea0e692a691f95d105
Jules Kourelakos <julia.kourelakos@duke.edu>
2023-02-23 20:35:00 -0500

    rules bug rpsls, part 5

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -41,7 +41,7 @@ if (args.r || args.rules) {
 try {
   console.log(JSON.stringify(rpsls(args._[0])));
 }
-  catch (error) {
-      console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:`);
-      process.exit(0);                      
-  }
+catch (error) {
+  console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:`);
+  process.exit(0);                      
+}
